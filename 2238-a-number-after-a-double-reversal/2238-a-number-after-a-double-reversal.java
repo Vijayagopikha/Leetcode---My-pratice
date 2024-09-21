7c42673aeac572650ee6a4adf122c5af8793f594
Vijayagopikha <115525092+Vijayagopikha@users.noreply.github.com>
2024-09-21 15:56:07 +0530

    Time: 0 ms (100%), Space: 40.2 MB (80.87%) - LeetHub

--- 2238-a-number-after-a-double-reversal/2238-a-number-after-a-double-reversal.java
+++ 2238-a-number-after-a-double-reversal/2238-a-number-after-a-double-reversal.java
@@ -1,6 +1,6 @@
 class Solution {
     public boolean isSameAfterReversals(int num) {
-        int n=num;
+       /* int n=num;
         for(int i=0;i<2;i++){
             int rev=0,b=0;
             while(num!=0){
@@ -13,6 +13,19 @@ class Solution {
         if(num==n)
         return true;
         else
+        return false;*/
+
+        
+        int n=num,ans=0;
+        while(n!=0)
+        {
+            ans=(ans*10)+n%10;
+            n=n/10;
+        } 
+        String s1=Integer.toString(num);
+        String s2=Integer.toString(ans);
+        if(s1.length()==s2.length())  return true;
         return false;
+
     }
 }
