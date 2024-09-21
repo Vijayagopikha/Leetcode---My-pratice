3554455e4d3d35be8d15b8365b423c2bd5ed95bb
Vijayagopikha <115525092+Vijayagopikha@users.noreply.github.com>
2024-09-21 21:20:30 +0530

    Time: 0 ms (100%), Space: 41.1 MB (9.66%) - LeetHub

--- 2491-smallest-even-multiple/2491-smallest-even-multiple.java
+++ 2491-smallest-even-multiple/2491-smallest-even-multiple.java
@@ -1,6 +1,8 @@
 
 class Solution {
     public int smallestEvenMultiple(int n) {
-        return (n%2==0)? n : n*2;
+        //return (n%2==0)? n : n*2;
+
+        return (n&1)==0 ? n: n*2;
     }
 }
