a2acbc71c93c5ec8cdafc1ec6d3ed03902624193
Vijaya Gopikha S <vijayagopikhas.22cse@kongu.edu>
2024-11-07 14:39:25 +0530

    Time: 4 ms (55.56%), Space: 11 MB (33.25%) - LeetHub

---
+++ 0496-next-greater-element-i/0496-next-greater-element-i.c
@@ -0,0 +1,30 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* nextGreaterElement(int* n1, int ns1, int* n2, int ns2, int* rs) {
+    *rs = ns1;
+    int *res = (int *)malloc(sizeof(int)*ns1);
+    int ind = 0, nt = -1,l;
+    for(int i=0;i<ns1;i++){
+        int found = 0;
+        for(int j=0;j<ns2;j++){
+            if(n1[i]==n2[j]){
+               for(int k=j+1;k<ns2;k++){
+                if(n2[k]>n2[j]){
+                   found = 1;
+                  l=k;
+                   break;
+                }
+               }
+               if(found==1){
+                res[ind++] = n2[l];
+               }
+               else{
+                res[ind++] = nt;
+               }
+              
+            }
+        }
+    }
+    return res;
+}
