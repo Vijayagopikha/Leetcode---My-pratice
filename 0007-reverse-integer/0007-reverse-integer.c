1052436d84d3224e4153e737e9a88fbb40c65d4d
Vijaya Gopikha S <vijayagopikhas.22cse@kongu.edu>
2024-11-07 22:34:47 +0530

    Time: 3 ms (52.84%), Space: 8.2 MB (93.34%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.c
+++ 0007-reverse-integer/0007-reverse-integer.c
@@ -1,14 +1,13 @@
 int reverse(int x){
     long int rev =0;
         while(x!=0){
-           int b=x%10; 
+           rev = rev * 10 +(x%10); 
              x/=10;
-        
-    if(rev<INT_MIN/10 || rev>INT_MAX/10){
+        }
+    if(rev<INT_MIN || rev>INT_MAX){
         return  0;
     }
     else
-       rev = rev *10 + b;
-        }
-        return rev;
+    return rev;
+
 }
