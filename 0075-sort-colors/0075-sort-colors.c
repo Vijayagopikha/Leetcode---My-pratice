58df9742097e762ced113d5cf325d11a981508e2
Vijaya Gopikha S <vijayagopikhas.22cse@kongu.edu>
2024-11-08 15:51:14 +0530

    Time: 0 ms (100%), Space: 9.5 MB (82.89%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.c
+++ 0075-sort-colors/0075-sort-colors.c
@@ -1,25 +1,26 @@
-void sortColors(int* a, int n) {
-    int zc = 0, oc = 0;
-    for(int i=0;i<n;i++){
-        if(a[i] == 0){
-            zc++;
-        }
-        if(a[i] == 1){
-            oc++;
-        }
+void sortColors(int* a, int ns) {
+    int zc=0,oc=0,tc=0;
+    for(int i=0;i<ns;i++)
+    {
+        if(a[i]==0)
+           zc++;
+        else if(a[i]==1)
+           oc++;     
     }
-   
-        for(int j=0;j<zc;j++){
-            a[j] = 0;
-            
 
-        }
-        for(int k=zc;k<(zc+oc);k++){
-            a[k] = 1;
-            
-        }
-        for(int i = (zc+oc);i<n;i++){
-            a[i] = 2;
-        }
-    
+
+    for(int i=0;i<zc;i++)
+    {
+        a[i]=0;
+       
+    }
+    for(int i=zc;i<zc+oc;i++)
+    {
+        a[i]=1;
+       
+    }
+    for(int i=zc+oc;i<ns;i++)
+    {
+        a[i]=2;
+    }
 }
