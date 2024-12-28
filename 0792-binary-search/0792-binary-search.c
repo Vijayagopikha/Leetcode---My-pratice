9b57b004ff5c8062a3a2148cd71ea383e5876aba
Vijaya Gopikha S <vijayagopikhas.22cse@kongu.edu>
2024-12-28 09:32:31 +0530

    Time: 0 ms (100%), Space: 9.3 MB (27.33%) - LeetHub

--- 0792-binary-search/0792-binary-search.c
+++ 0792-binary-search/0792-binary-search.c
@@ -1,9 +1,28 @@
 int search(int* nums, int n, int target) {
-    for(int i=0;i<n;i++){
+   /* for(int i=0;i<n;i++){
        if(nums[i] == target){
         
         return i;
        }
     }
-   return -1;
+   return -1;*/
+    int l = 0;
+    int r = n-1;
+    int mid = (l+r)/2;
+    while(l<=r){
+    if(nums[mid] ==  target){
+        return mid;
+    }
+    if(target < nums[mid]){
+        r = mid-1;
+        mid = (l+r)/2;
+
+    }
+    else{
+        l = mid+1;
+        mid = (l+r)/2;
+    }
+    }
+    return -1;
+ 
 }
