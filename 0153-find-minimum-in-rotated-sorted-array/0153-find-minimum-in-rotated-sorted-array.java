a7018988c68927f1e21eba1a7171fd2b7258f3cc
Vijaya Gopikha S <vijayagopikhas.22cse@kongu.edu>
2024-12-28 14:40:10 +0530

    Time: 0 ms (100%), Space: 42 MB (60.04%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.java
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.java
@@ -11,7 +11,9 @@ class Solution {
             }
         }
         return nums[left];*/
-        if(nums[0] < nums[right]) return nums[0];
+
+        //note
+        /*if(nums[0] < nums[right]) return nums[0];
         while(left < right){
             int mid = (left+right)/2;
             if(nums[mid]>nums[mid+1]){
@@ -27,6 +29,18 @@ class Solution {
                 else{
                     left = mid+1;
                 }
+            }
+        }
+        return nums[left];*/
+          
+        if(nums[0] < nums[right]) return nums[0];
+        while(left<=right){
+            int mid = (left+right)/2;
+            if(nums[mid]<=nums[nums.length-1]){
+                right = mid-1;
+            }
+            else{
+                left = mid +1;
             }
         }
         return nums[left];
