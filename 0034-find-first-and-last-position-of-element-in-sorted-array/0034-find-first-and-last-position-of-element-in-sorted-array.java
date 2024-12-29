5e5f6ece04822e042a28f94133e46e366a3aa9c3
Vijaya Gopikha S <vijayagopikhas.22cse@kongu.edu>
2024-12-29 14:35:24 +0530

    Time: 0 ms (100%), Space: 45.9 MB (51.11%) - LeetHub

---
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -0,0 +1,60 @@
+class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        int n = nums.length;
+        int left = 0;
+        int right = n-1;
+        int []res = new int[2];
+        res[0] = -1;
+        res[1] = -1;
+        while(left<=right){
+            int mid = (left+right)/2;
+           
+            if(nums[mid] == target){
+                res[0] = mid;
+                res[1] = mid;
+                int  i = mid-1;
+                while(i>=0 && (nums[mid] == nums[i])){
+                    res[0] = i;
+                    i--;
+                }
+                int j = mid+1;
+                while(j <n && (nums[j] ==  nums[mid])){
+                    res[1] = j;
+                    j++;
+                }
+                
+                return res;
+                
+               
+                /* else if(mid-1 >=0   && mid+1<n &&(nums[mid-1] ==  nums[mid+1]) ){
+                    res[0] = mid -1;
+                    res[1] = mid+1;
+                    return res;
+                }
+                 else if(mid+1 <n && (nums[mid+1] ==  nums[mid])){
+                    res[0] = mid;
+                    res[1] = mid+1;
+                    return res;
+                }
+                else if(mid-1 >=0 && (nums[mid-1] ==  nums[mid])){
+                    res[0] = mid-1;
+                    res[1] = mid;
+                    return res;
+                }
+               
+                else{
+                    res[0] = mid;
+                    res[1] = mid;
+                    return res;
+                }*/
+            }
+            else if (nums[mid] < target){
+                 left = mid+1;
+            }
+            else{
+                right = mid-1;
+            }
+        }
+        return res;
+    }
+}
