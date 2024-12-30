15e97e01d9130e6525e04ba54f8edb5eb7e3d2d6
Vijaya Gopikha S <vijayagopikhas.22cse@kongu.edu>
2024-12-30 14:16:59 +0530

    Time: 0 ms (100%), Space: 8.5 MB (45.82%) - LeetHub

--- 0908-middle-of-the-linked-list/0908-middle-of-the-linked-list.c
+++ 0908-middle-of-the-linked-list/0908-middle-of-the-linked-list.c
@@ -7,21 +7,15 @@
  */
 struct ListNode* middleNode(struct ListNode* head) {
     struct ListNode* current = head;
-    int c=0;
+     int count = 0;
     while(current){
-        c++;
         current = current->next;
+        count++;
     }
-    int mid=c/2;
-   
-   /* int* result = (int*)malloc((c-mid+1) * sizeof(int));
-    int j=0;
-*/
+    int mid = count/2;
     for(int i=0;i<mid;i++){
         head = head->next;
     }
-  
-   
     return head;
     
 }
