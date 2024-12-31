5f522f18ef030d150f32131490de1bf128d9b08d
Vijaya Gopikha S <vijayagopikhas.22cse@kongu.edu>
2024-12-31 09:33:33 +0530

    Time: 0 ms (100%), Space: 7.8 MB (42.16%) - LeetHub

--- 0202-happy-number/0202-happy-number.c
+++ 0202-happy-number/0202-happy-number.c
@@ -1,6 +1,6 @@
 bool isHappy(int n) {
-    int i=0;
-    while(i++<10){
+    /*int i=0;
+    while(i++<n){
         int sum = 0;
         while(n!=0){
             int num = n%10;
@@ -14,5 +14,24 @@ bool isHappy(int n) {
          n = sum;
       
     }
-    return false;
+    return false;*/
+
+
+    //n = 1 la end ahna happy ...not happy means its end with 4 (its fact)
+
+    do{
+         int sum = 0;
+          while(n!=0){
+            int num = n%10;
+            sum+=num*num;
+            n/=10;
+        }
+        n=sum;
+    }while(n!=1 && n!=4);
+    if(n==1){
+        return true;
+    }
+    else{
+        return false;
+    }
 }
