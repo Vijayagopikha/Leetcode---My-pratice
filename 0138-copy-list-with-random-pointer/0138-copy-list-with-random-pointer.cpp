80feed94425117e7c8456422aa3b2c9fc57f92a0
Vijaya Gopikha S <vijayagopikhas.22cse@kongu.edu>
2025-01-01 16:33:32 +0530

    Time: 0 ms (100%), Space: 15.1 MB (56.83%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
@@ -17,13 +17,12 @@ public:
 class Solution {
 public:
     Node* copyRandomList(Node* head) {
-        unordered_map<Node*, Node*>m;
+       /* unordered_map<Node*, Node*>m;
         Node *temp = head;
         while(temp!=NULL){
             Node *newn = new Node(temp->val);
             m[temp] = newn;
             temp = temp->next;
-
         }
         temp = head;
         while(temp!=NULL){
@@ -31,6 +30,37 @@ public:
              m[temp]->random = m[temp->random];
              temp = temp->next;
         }
-        return m[head];
+        return m[head];*/
+        if (!head) return NULL;
+        Node *temp = head;
+        while(temp!=NULL){
+            Node *newn = new Node(temp->val);
+            newn->next = temp->next;
+            temp->next = newn;
+            temp = temp->next->next;
+            
+        }
+        Node *th1 = head;
+       while(th1!=NULL){
+        if(th1->random != NULL){
+            th1->next->random = th1->random->next;
+        }
+        th1 = th1->next->next;
+       }
+        th1 = head;
+        Node *th2 = head->next;
+        Node *newHead = head->next;
+        while(th1!=NULL){
+           th1->next = th1->next->next;
+           if(th2->next != NULL){
+              
+           th2->next = th2->next->next;
+           }
+           th1 = th1->next;
+           th2 = th2->next;
+        }
+        
+         return newHead;
+
     }
 };
