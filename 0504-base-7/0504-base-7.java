6cabe35bebe5668f252c924df0e4d363b741d4c1
Vijaya Gopikha S <vijayagopikhas.22cse@kongu.edu>
2025-01-03 18:42:15 +0530

    Time: 1 ms (63.25%), Space: 41.6 MB (13.95%) - LeetHub

--- 0504-base-7/0504-base-7.java
+++ 0504-base-7/0504-base-7.java
@@ -3,10 +3,9 @@ class Solution {
         StringBuilder s = new StringBuilder();
         int[] a=new int[100];
         int k = 0;
-        if(num==0){
-            s.append('0');
-            return s.toString();
-        }
+        if(num<7 && num>-7) return Integer.toString(num);
+        if(num==7) return Integer.toString(10);
+        if(num==-7) return Integer.toString(-10);
         if(num<0){
             num = Math.abs(num);
             s.append('-');
@@ -19,6 +18,27 @@ class Solution {
         for(int i=k-1;i>=0;i--){
             s.append(a[i]);
         }
-        return s.toString();
+        return s.toString(); 
+       /* if(num<7 && num>-7) return Integer.toString(num);
+        if(num==7) return Integer.toString(10);
+        if(num==-7) return Integer.toString(-10);
+        int sum = 0;
+        if(num<0){
+            num = Math.abs(num);
+            while(num!=0){
+            sum += (num%7)*10;
+           num/=7;
+        }
+        return Integer.toString(-1*sum);
+        }
+        else{
+           
+            while(num!=0){
+            sum += (num%7)*10;
+           num/=7;
+        }
+        return Integer.toString(sum);
+        }*/
+
     }
 }
