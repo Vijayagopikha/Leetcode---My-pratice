4f406aa74e6dd6a48d8fe4bd4aee98852e22f626
Vijaya Gopikha S <vijayagopikhas.22cse@kongu.edu>
2025-01-06 19:47:52 +0530

    Time: 0 ms (100%), Space: 9.6 MB (26.01%) - LeetHub

--- 0059-spiral-matrix-ii/0059-spiral-matrix-ii.cpp
+++ 0059-spiral-matrix-ii/0059-spiral-matrix-ii.cpp
@@ -1,4 +1,4 @@
-/*class Solution {
+class Solution {
 public:
     vector<vector<int>> generateMatrix(int n) {
         
@@ -8,7 +8,11 @@ public:
         int cb = 0;
         int ce = n-1;
         int k = 1;
-        int arr[n][n];
+        //int arr[n][n];
+        int **arr = new int*[n];
+        for(int i=0;i<n;i++){
+            arr[i] = new int[n];
+        }
         while(rb<=re && cb<=ce && k<=n*n){
           
             for(int i = cb;i<=ce;i++){
@@ -41,42 +45,45 @@ public:
         }
         for(int i=0;i<n;i++){
             vector<int>v;
-            for(int j=0j;<n;j++){
+            for(int j=0;j<n;j++){
               v.push_back(arr[i][j]);
             }
             res.push_back(v);
         }
         return res;
     }
-};*/
+};
 
-class Solution {
+/*class Solution {
 public:
     vector<vector<int>> generateMatrix(int n) {
         vector<vector<int>> res(n, vector<int>(n));
-        int rb = 0, re = n - 1, cb = 0, ce = n - 1;
+        int rb = 0;
+        int  re = n - 1;
+        int cb = 0;
+        int ce = n - 1;
         int k = 1;
 
         while (rb <= re && cb <= ce) {
-            for (int i = cb; i <= ce; ++i) {
+            for (int i = cb; i <= ce; i++) {
                 res[rb][i] = k++;
             }
             rb++;
             
-            for (int i = rb; i <= re; ++i) {
+            for (int i = rb; i <= re; i++) {
                 res[i][ce] = k++;
             }
             ce--;
             
             if (rb <= re) {
-                for (int i = ce; i >= cb; --i) {
+                for (int i = ce; i >= cb; i--) {
                     res[re][i] = k++;
                 }
             }
             re--;
             
             if (cb <= ce) {
-                for (int i = re; i >= rb; --i) {
+                for (int i = re; i >= rb; i--) {
                     res[i][cb] = k++;
                 }
             }
@@ -86,3 +93,4 @@ public:
         return res;
     }
 };
+*/
