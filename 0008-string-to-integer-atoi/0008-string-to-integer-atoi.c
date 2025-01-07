6c0e218057081afc78f594991f4704c6fe0f7c54
Vijaya Gopikha S <vijayagopikhas.22cse@kongu.edu>
2025-01-07 18:35:07 +0530

    Time: 0 ms (100%), Space: 8.7 MB (18.5%) - LeetHub

---
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.c
@@ -0,0 +1,21 @@
+int myAtoi(char* s) {
+    int i=0;
+    int sign=1,r=0;
+    while(isspace(s[i])){
+        i++;
+    }
+    if(s[i]=='-' || s[i]=='+'){
+        sign = (s[i]=='-')?-1:1;
+        i++;
+    }
+    while(isdigit(s[i])){
+        int d = s[i]-'0';
+        if(r > (INT_MAX-d)/10){
+            return (sign==1)?INT_MAX:INT_MIN;
+        }
+
+        r  = r*10+d;
+        i++;
+    }
+    return r*sign;
+}
