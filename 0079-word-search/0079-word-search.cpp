b184e1307d8a39f8756b954be265434eb142716a
Vijaya Gopikha S <vijayagopikhas.22cse@kongu.edu>
2025-01-08 19:00:01 +0530

    Time: 190 ms (91.7%), Space: 10.3 MB (89.18%) - LeetHub

---
+++ 0079-word-search/0079-word-search.cpp
@@ -0,0 +1,66 @@
+/*class Solution {
+public:
+    bool exist(vector<vector<char>>& board, string word) {
+        unordered_map<char,int>m;
+        int n = board.size();
+        int m1 = board[0].size();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m1;j++){
+                m[board[i][j]] = m[board[i][j]] + 1;
+            }
+        }
+        int l = word.size();
+        for(int i=0;i<l;i++){
+            if(m[word[i]]==0){
+                cout<<word[i];
+                cout<<m[word[i]];
+                return false;
+            }
+            m[word[i]] -=1;
+        }
+        return true;
+    }
+};*/
+
+class Solution {
+public:
+    bool exist(std::vector<std::vector<char>>& board, std::string word) {
+        int n = board.size();
+        int m = board[0].size();
+
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < m; ++j) {
+                if (board[i][j] == word[0] && backtrack(board, word, i, j, 0)) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+private:
+    bool backtrack(std::vector<std::vector<char>>& board, const std::string& word, int row, int col, int index) {
+        int n = board.size();
+        int m = board[0].size();
+
+        if (index == word.size()) {
+            return true; // All characters matched
+        }
+        if (row < 0 || row >= n || col < 0 || col >= m || board[row][col] != word[index]) {
+            return false;
+        }
+
+        char temp = board[row][col];
+        board[row][col] = '#'; // Mark the cell as visited
+
+        // Explore all four possible directions (up, down, left, right)
+        bool found = backtrack(board, word, row + 1, col, index + 1) ||
+                     backtrack(board, word, row - 1, col, index + 1) ||
+                     backtrack(board, word, row, col + 1, index + 1) ||
+                     backtrack(board, word, row, col - 1, index + 1);
+
+        board[row][col] = temp; // Unmark the cell
+        return found;
+    }
+};
