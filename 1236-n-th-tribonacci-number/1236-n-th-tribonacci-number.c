22b68ac14af63e1959b99f89ba0f083f0b753c6a
Vijaya Gopikha S <vijayagopikhas.22cse@kongu.edu>
2025-01-21 15:34:14 +0530

    Time: 0 ms (100%), Space: 7.7 MB (56.3%) - LeetHub

--- 1236-n-th-tribonacci-number/1236-n-th-tribonacci-number.c
+++ 1236-n-th-tribonacci-number/1236-n-th-tribonacci-number.c
@@ -1,8 +1,19 @@
-int tribonacci(int n) {
+int sol(int  n, int *dp){
     if(n==0)
        return 0;
     if(n==1 || n==2)
       return 1;
+    if(dp[n]!=-1)
+      return dp[n];
+    
+    return dp[n] = sol(n-1,dp) + sol(n-2,dp)+ sol(n-3,dp);
+ }
+int tribonacci(int n) {
+    /*if(n==0)
+       return 0;
+    if(n==1 || n==2)
+      return 1;*/
+
    /* int t1 = 0;
     int t2 = 1;
     int t3 = 1;
@@ -14,13 +25,18 @@ int tribonacci(int n) {
         t3 = c;
     }
     return c;*/
+  
+  
     int dp[38];
     memset(dp,-1,sizeof(dp));
-    dp[0] = 0;
+
+    return sol(n,dp);
+    /*dp[0] = 0;
     dp[1] = 1;
     dp[2] = 1;
     for(int i=3;i<=n;i++){
         dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
     }
-   return dp[n];
+   return dp[n];*/
+
 }
