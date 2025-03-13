712f59cc5142648f274b7e5b51a0758e2c89ffec
Vijaya Gopikha S <vijayagopikhas.22cse@kongu.edu>
2025-03-13 05:35:52 +0530

    Time: 0 ms (100%), Space: 41.8 MB (41.58%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        int n = strs.length;
+        String p = strs[0];
+        for(int i=1;i<n;i++){
+            while(strs[i].indexOf(p) != 0){
+                p = p.substring(0,p.length()-1);
+
+                if(p.isEmpty()){
+                    return "";
+                }
+            }
+        }
+        return p;
+    }
+}
