adb2fe0b86b255fe20338436d2ac6d348edafb2b
Vijaya Gopikha S <vijayagopikhas.22cse@kongu.edu>
2025-03-13 07:14:25 +0530

    Time: 4 ms (4.06%), Space: 42.5 MB (55.36%) - LeetHub

---
+++ 1310-watering-plants/1310-watering-plants.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int wateringPlants(int[] plants, int capacity) {
+        int c =  capacity;
+        int step = 0;
+        int l = plants.length;
+        int i=0;
+        while(i<l){
+           if(plants[i]<=capacity){
+            capacity-=plants[i];
+            step++;
+            i++;
+           }
+           else{
+            capacity = c;
+            for(int j=i-1;j>=0;j--){
+                step++;
+            }
+           
+            for(int j=0;j<i;j++){
+               
+                step++;
+            }
+           
+           }
+           
+        }
+        return step;
+    }
+}
