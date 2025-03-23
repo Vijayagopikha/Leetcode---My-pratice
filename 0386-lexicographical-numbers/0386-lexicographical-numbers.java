e71a31de12a41e99669ca90682bca802b60ddeae
Vijaya Gopikha S <vijayagopikhas.22cse@kongu.edu>
2025-03-23 12:59:18 +0530

    Time: 4 ms (68.9%), Space: 48.8 MB (19.32%) - LeetHub

---
+++ 0386-lexicographical-numbers/0386-lexicographical-numbers.java
@@ -0,0 +1,18 @@
+class Solution {
+    public List<Integer> lexicalOrder(int n) {
+       List<Integer>  res = new ArrayList<>();
+       for(int i = 1;i<=9;i++){
+        dfs(i,n,res);
+       }
+       return res;
+    }
+    public void dfs(int cur , int n, List<Integer>res){
+        if(cur >n)
+         return;
+        res.add(cur);
+        for(int i=0;i<=9;i++){
+            int next = cur*10 + i;
+            dfs(next, n, res);
+        }
+    }
+}
